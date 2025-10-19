9b8afbaf266dcc5a08bbae9b6360e5925088cbe9
monylangmading-ops <monylangmading@gmail.com>
2025-10-18 21:45:10 -0700

    Update all changes to script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const quotes = [
 ];
 
 function displayRandomQuote() {
-  const quoteDisplay = document.getElementById('quoteDisplay');
+  const quoteDisplay = document.getElementById('quoteDisplay').innerHTML;
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
   quoteDisplay.innerHTML = randomQuote.text + " — " + randomQuote.category;
@@ -26,5 +26,5 @@ function addQuote() {
   alert("Quote added successfully!");
 }
 
-document.getElementById('newQuote').addEventListener('click', displayRandomQuote);
+document.getElementById('newQuote').addEventListener('click', displayRandomQuote.innerHTML);
 document.getElementById('addQuoteBtn').addEventListener('click', addQuote);
